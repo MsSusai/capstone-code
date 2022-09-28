0a5c7f18f149ba832b8a5d07fda841b3177b1ae0
MsSusai <liuhaorantony@163.com>
2022-09-28 21:54:56 +0800

    commit

--- example_script/python_script/anova.py
+++ example_script/python_script/anova.py
@@ -33,110 +33,112 @@ for line in open(sys.argv[1], "r"):
 		else:
 			dir[type].append(float(l[3]))
 	n += 1
-print(df)
-# print(dir)
-
-#
-# m = 0
-# for line in open(phyfile, "r"):
-# 	m += 1
-#
-# print("flag=" + str(int(m) * float(flag)))
-#
-# typenum = len(dir)
-# print(typenum)
-# nn = 0
-# for k, v in list(dir.items()):
-# 	if len(v) >= int(m) * float(flag):
-# 		# if len(v)>=87:
-# 		nn += 1
-# 	else:
-# 		del dir[k]
-#
-# 		df = df[~df['Type'].isin([k])]
-#
-# print(dir)
-# print(len(dir))
-# cha = typenum - len(dir)
-#
 # print(df)
-#
-# filer = []
-# print("There are " + str(
-# 	cha) + " haplotypes that contain less than 5 materials and are filtered, and the remaining " + str(
-# 	len(dir)) + " haplotypes are used for subsequent analysis.")
-# filer.append("all: " + str(typenum) + "\n")
-# filer.append("filter: " + str(cha) + "\n")
-# filer.append("remain: " + str(len(dir)) + "\n")
+# print(dir)
 
-# if len(dir) > 1:
-# 	final = []
-# 	normalfile = []
-# 	n = 0
-# 	m = 0
-# 	for v in list(dir.values()):
-# 		value = np.array(v)
-# 		test = kstest(value, 'norm')
-# 		m += 1
-# 		normalfile.append("kstest pvalue: " + str(test[1]) + "\n")
-# 		if test[1] <= 0.05:
-# 			n += 1
-#
-# 	mylist = []
-# 	# mylistk = []
-# 	# mylistv = []
-# 	string = []
-# 	levenes = []
-# 	head = ""
-# 	if m == n:
-# 		for v1 in list(dir.values()):
-# 			mylist.append(v1)
-# 		# print mylist
-#
-# 		# for k1 in dir.keys():
-# 		# for v1 in dir[k1]:
-# 		# mylistv.append(v1)
-# 		# mylistk.append(k1)
-# 		stat, p = levene(*mylist)
-# 		levenes.append("levene Fvalue: " + str(stat) + "\n")
-# 		levenes.append("levene pvalue: " + str(p) + "\n")
-# 		if p > 0.01:
-# 			model = ols('Value~C(Type)', data=df).fit()
-# 			anova_table = anova_lm(model, typ=2)
-# 			print(anova_table)
-# 			PR = anova_table.loc['C(Type)', 'PR(>F)']
-# 			F_value = anova_table.loc['C(Type)', 'F']
-# 			tablename = "./anova_table" + "_" + phy + "_" + name + ".txt"
-# 			anova_table.to_csv(tablename, sep="\t", na_rep="NA")
-# 			if PR < 0.05:
-# 				file1name = "./anova_result" + "_" + phy + "_" + name + ".txt"
-# 				file1 = open(file1name, "w")
-# 				file1.write("name\tphenotype\tgroup1\tgroup2\tF_value\tp_value\tmeandiff\tp-adj\n")
-# 				mc = pairwise_tukeyhsd(df['Value'], df['Type'], alpha=0.05)
-# 				print(df['Value'])
-# 				print(df['Type'])
-# 				# print mylistv
-# 				# print pd.Series(mylistv)
-#
-# 				# print pd.Series(mylistk)
-# 				# print mylistk
-# 				# mc = pairwise_tukeyhsd(pd.Series(mylistv),pd.Series(mylistk),alpha=0.05)
-#
-# 				# mc = MultiComparison(pd.Series(mylistv),pd.Series(mylistk))
-# 				# print mc
-# 				# tukey_result1 = mc.tukeyhsd(alpha = 0.05)
-# 				# print tukey_result1.summary()
-# 				print(mc)
-# 				tukey_result = mc._results_table.data[1:]
-# 				# print (tukey_result)
-# 				for line in tukey_result:
-# 					# if line[-1]=="True":
-# 					file1.write(name + "\t" + phy + "\t" + line[0] + "\t" + line[1] + "\t" + str(F_value) + "\t" + str(
-# 						PR) + "\t" + str(line[2]) + "\t" + str(line[3]) + "\n")
-# 				# print tukey_result
-# 				# print mc.summary()
-# 			else:
-# 				print("There is no difference in the average phenotype of the two haplotypes.")
+
+m = 0
+for line in open(phyfile, "r"):
+	m += 1
+
+print("flag=" + str(int(m) * float(flag)))# 保留不少于规定数目的单倍型
+
+typenum = len(dir)
+print(typenum)
+nn = 0
+for k, v in list(dir.items()):
+	if len(v) >= int(m) * float(flag): # 保留不少于规定数目的单倍型（300*0.05=15）
+		# if len(v)>=87:
+		nn += 1
+	else:
+		del dir[k]
+
+		df = df[~df['Type'].isin([k])]  # 将少于15个样本的单倍型剔除
+print(dir)
+print(len(dir))
+cha = typenum - len(dir)
+
+print(df)
+
+filer = []
+print("There are " + str(
+	cha) + " haplotypes that contain less than 5 materials and are filtered, and the remaining " + str(
+	len(dir)) + " haplotypes are used for subsequent analysis.")
+filer.append("all: " + str(typenum) + "\n")
+filer.append("filter: " + str(cha) + "\n")
+filer.append("remain: " + str(len(dir)) + "\n")
+
+
+
+if len(dir) > 1:
+	final = []
+	normalfile = []
+	n = 0
+	m = 0
+	for v in list(dir.values()):
+		value = np.array(v)
+		test = kstest(value, 'norm')
+		m += 1
+		normalfile.append("kstest pvalue: " + str(test[1]) + "\n")
+		if test[1] <= 0.05:
+			n += 1
+	print(normalfile)
+	
+	# mylist = []
+	# # mylistk = []
+	# # mylistv = []
+	# string = []
+	# levenes = []
+	# head = ""
+	# if m == n:
+	# 	for v1 in list(dir.values()):
+	# 		mylist.append(v1)
+	# 	# print mylist
+	# 	print(mylist)
+		# for k1 in dir.keys():
+		# for v1 in dir[k1]:
+		# mylistv.append(v1)
+		# mylistk.append(k1)
+	# 	stat, p = levene(*mylist)
+	# 	levenes.append("levene Fvalue: " + str(stat) + "\n")
+	# 	levenes.append("levene pvalue: " + str(p) + "\n")
+	# 	if p > 0.01:
+	# 		model = ols('Value~C(Type)', data=df).fit()
+	# 		anova_table = anova_lm(model, typ=2)
+	# 		print(anova_table)
+	# 		PR = anova_table.loc['C(Type)', 'PR(>F)']
+	# 		F_value = anova_table.loc['C(Type)', 'F']
+	# 		tablename = "./anova_table" + "_" + phy + "_" + name + ".txt"
+	# 		anova_table.to_csv(tablename, sep="\t", na_rep="NA")
+	# 		if PR < 0.05:
+	# 			file1name = "./anova_result" + "_" + phy + "_" + name + ".txt"
+	# 			file1 = open(file1name, "w")
+	# 			file1.write("name\tphenotype\tgroup1\tgroup2\tF_value\tp_value\tmeandiff\tp-adj\n")
+	# 			mc = pairwise_tukeyhsd(df['Value'], df['Type'], alpha=0.05)
+	# 			print(df['Value'])
+	# 			print(df['Type'])
+	# 			# print mylistv
+	# 			# print pd.Series(mylistv)
+	#
+	# 			# print pd.Series(mylistk)
+	# 			# print mylistk
+	# 			# mc = pairwise_tukeyhsd(pd.Series(mylistv),pd.Series(mylistk),alpha=0.05)
+	#
+	# 			# mc = MultiComparison(pd.Series(mylistv),pd.Series(mylistk))
+	# 			# print mc
+	# 			# tukey_result1 = mc.tukeyhsd(alpha = 0.05)
+	# 			# print tukey_result1.summary()
+	# 			print(mc)
+	# 			tukey_result = mc._results_table.data[1:]
+	# 			# print (tukey_result)
+	# 			for line in tukey_result:
+	# 				# if line[-1]=="True":
+	# 				file1.write(name + "\t" + phy + "\t" + line[0] + "\t" + line[1] + "\t" + str(F_value) + "\t" + str(
+	# 					PR) + "\t" + str(line[2]) + "\t" + str(line[3]) + "\n")
+	# 			# print tukey_result
+	# 			# print mc.summary()
+	# 		else:
+	# 			print("There is no difference in the average phenotype of the two haplotypes.")
 # 		# else:
 # 		# #filepath="/home/riceUsers/xueai/soybean/SNP/variance.txt"
 # 		# #file=open(filepath,"a")
@@ -169,7 +171,7 @@ print(df)
 # 	print("There is only 1 group of haplotypes or less, so the gene does not perform analysis of variance.")
 #
 # print(name + ".........finish!!!!!")
-#
-# w3 = open(output3, "w")
-# w3.writelines(filer)
-# w3.close()
+
+w3 = open(output3, "w")
+w3.writelines(filer)
+w3.close()
--- my_script/python/anova.py
+++ my_script/python/anova.py
@@ -6,6 +6,10 @@
 # 工具：PyCharm
 # 检验数据正态分布（kstest，p>0.05）、方差齐性（levene,p>0.01）最后做方差分析（F），多重比较（p<0.05）
 
+# 运行：
+# windows：python .\my_script\python\anova.py .\sample_file\Glyma.20G250200_protein_2015yzBL_hap2phe.csv .\sample_file\Pro_content.txt Pro_content 2015yzBL Glyma.20G250200 0.05 normal_result_Glyma.20G250200_2015_Oil.txt variances_result_Glyma.20G250200_2015_Pro.txt filter_result_Glyma.20G250200_2015_Pro.txt
+
+
 import sys
 import pathlib
 from typing import Union
@@ -20,23 +24,64 @@ from statsmodels.stats.anova import anova_lm
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 
-def anova_filter(haplotype_df: pd.DataFrame):
-	pass
+def anova_filter(haplotype_df: pd.DataFrame, parameter: str) -> pd.DataFrame:
+	"""
+	过滤样本单倍型
+	单倍型包含数据数目>总材料数目*parameter
+	"""
+	# 保留不少于规定数目的单倍型（219*0.05=10.9）
+	df_filter = pd.Series(haplotype_df["type"].value_counts() >= len(haplotype_df) * float(parameter))
+	for hap, values in df_filter.items():
+		if not values:
+			# 将少于10.9个样本的单倍型从表中剔除
+			haplotype_df = haplotype_df[~haplotype_df["type"].isin([hap])]
+	return haplotype_df
+
+
+def normality_test(filtered_haplotype_df: pd.DataFrame):
+	"""
+	正态分布检验
+	使用 K-S test
+	"""
+	hap_series = pd.Series(filtered_haplotype_df["type"].value_counts())
+	for hap in hap_series.keys():
+		hap_df = filtered_haplotype_df[filtered_haplotype_df["type"] == hap]
+		mean = hap_df["value"].mean()
+		std = hap_df["value"].std()
+		print(kstest(hap_df["value"], 'norm', (mean, std))) # p>0.05符合正态分布
+		
 
 
 def main():
 	# 输入文件与参数
-	haplotype_df: pd.DataFrame = pd.read_csv(sys.argv[1], header=0, index_col=None)  # 单倍型数据
-	phenotype_file: Union[str, pathlib.Path] = sys.argv[2]  # 表型数据
+	haplotype_df: pd.DataFrame = pd.read_csv(sys.argv[1], index_col=0)  # 单倍型数据
+	phenotype_path: Union[str, pathlib.Path] = sys.argv[2]  # 表型数据
 	phenotype: str = sys.argv[3]  # 表型
 	year_flag: str = sys.argv[4]  # 年份
 	gene_accession: str = sys.argv[5]  # 基因号
-	p_value: str = sys.argv[6]  # p值
+	parameter: str = sys.argv[6]  # 规定的参数，作用是:样本总数目*parameter=保留不少于规定数目的单倍型
 	
 	# 输出文件
-	kstest_file: [str, pathlib.Path] = sys.argv[7]  # 正态分布检验结果
-	levene_file: [str, pathlib.Path] = sys.argv[8]  # 方差齐性检验结果
-	filter_file: [str, pathlib.Path] = sys.argv[9]  # 过滤文件检验结果
+	kstest_path: [str, pathlib.Path] = sys.argv[7]  # 正态分布检验文件路径
+	levene_path: [str, pathlib.Path] = sys.argv[8]  # 方差齐性检验文件路径
+	filter_path: [str, pathlib.Path] = sys.argv[9]  # 过滤文件路径
+	filter_list: list = []  # 过滤文件检验结果
+	
+	filtered_haplotype_df = anova_filter(haplotype_df, parameter)  # 过滤满足条件的单倍型
+	# print("There are "
+	#       + str(len(haplotype_df["type"].value_counts()) - len(filtered_haplotype_df["type"].value_counts())) +
+	#       " haplotypes that contain less than 5 materials and are filtered, and the remaining "
+	#       + str(len(filtered_haplotype_df["type"].value_counts())) +
+	#       " haplotypes are used for subsequent analysis.")
+	# # 输出过滤文件检验结果
+	# filter_list.append("all haplotype: " + str(len(haplotype_df["type"].value_counts())) + "\n")
+	# filter_list.append("filtered haplotype: " + str(
+	# 	len(haplotype_df["type"].value_counts()) - len(filtered_haplotype_df["type"].value_counts())) + "\n")
+	# filter_list.append("remain haplotype " + str(len(filtered_haplotype_df["type"].value_counts())) + "\n")
+	# with open(filter_path, "w") as filter_file:
+	# 	filter_file.writelines(filter_list)
+	
+	normality_test(filtered_haplotype_df)
 
 
 if __name__ == '__main__':
