fe0ea9c4d8bb758cecca01cb383eb9211a928a43
MsSusai <liuhaorantony@163.com>
2022-11-01 21:44:29 +0800

    commit

---
+++ my_script/shell/old_snp/hapdata_extract.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# 单倍型分析表格
+# /home/riceUsers/lhr/soybean/output/${phenotype}_old_no_normaltest/${year}/${gene}_${phenotype}_${year}_hap2phe.csv
+
+# 多重比较结果
+# /home/riceUsers/lhr/soybean/output/${phenotype}_old_no_normaltest/${year}/multiple_comparison_result_${gene}_${year}_${phenotype}.csv
+
+
+for phenotype in $(cat /home/riceUsers/lhr/soybean/genomefile/phenotype.txt)
+do
+  cd /home/riceUsers/lhr/soybean/final_results/haplotype/
+  mkdir ${phenotype}
+  cd ${phenotype}
+  for year in 2015yzBL 2016yzBL 2017yzBL
+  do
+    echo "正在处理${phenotype}_${year}"
+    mkdir ${year}
+    cd ${year}
+    for gene in $(cat /home/riceUsers/lhr/soybean/final_results/gene_loc/${phenotype}.txt)
+    do
+      cp /home/riceUsers/lhr/soybean/output/${phenotype}_old_no_normaltest/${year}/${gene}_${phenotype}_${year}_hap2phe.csv .
+      cp /home/riceUsers/lhr/soybean/output/${phenotype}_old_no_normaltest/${year}/multiple_comparison_result_${gene}_${year}_${phenotype}.csv .
+    done
+    cd ..
+  done
+  cd ..
+done
