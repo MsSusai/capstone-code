2b4c4071daff0e9583cde0304d299686d4db855d
MsSusai <liuhaorantony@163.com>
2022-11-10 17:13:44 +0800

    commit

---
+++ my_script/python/add_samplelist_count.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+
+# 作者：Sosai
+# 时间：2022/11/10  15:33 
+# 名称：add_samplelist_count.py
+# 工具：PyCharm
+import pandas as pd
+import os
+
+countlist = []
+
+path = "../../result/hapdata_for_database/output/"
+files = os.listdir(path)
+for file in files:
+    filepath = path + file
+    df = pd.read_csv(filepath)
+    for line in df["Sample List"]:
+        count = line.split(',')
+        countlist.append(len(count))
+    df["Sample Count"] = countlist
+    countlist = []
+    df.to_csv(file)
