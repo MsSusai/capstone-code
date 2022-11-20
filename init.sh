62331135ac2cf6322d4a880072e62164ab5637c1
MsSusai <liuhaorantony@163.com>
2022-11-20 17:46:31 +0800

    commit

--- init.sh
+++
@@ -1,7 +0,0 @@
-echo "# capstone-code" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:MsSusai/capstone-code.git
-git push -u origin main
