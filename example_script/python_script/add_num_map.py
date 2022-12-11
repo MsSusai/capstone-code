018c0700482ec5a77bb13246c9f3867e03805248
MsSusai <liuhaorantony@163.com>
2022-12-11 16:07:47 +0800

    commit

--- example_script/python_script/add_num_map.py
+++
@@ -1,9 +0,0 @@
-# python $scripts/add_num_map.py genelocation_rangesnp.map genelocation_rangesnp_new.map 
-
-import sys
-
-n=0
-for line in open(sys.argv[1],"r"):
-    l = line.strip().split("\t")
-    print ("\t".join(l[:])+"\t"+str(n))
-    n+=1
