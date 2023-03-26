f04b2167c9a1de626b837d8c8ddd399997bfd64f
MsSusai <liuhaorantony@163.com>
2023-03-26 23:07:51 +0800

    commit

--- script/shell/final_hapdata.sh
+++ script/shell/final_hapdata.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # nohup sh final_hapdata.sh > final_hapdata.log 2>&1 &
 
-for phenotype in $(cat /home/riceUsers/lhr/soybean/genomefile/phenotype.txt)
-do
+#for phenotype in $(cat /home/riceUsers/lhr/soybean/genomefile/phenotype.txt)
+phenotype="grain_weight"
+#do
   for gene in $(cat /home/riceUsers/lhr/soybean/final_results/gene_loc/${phenotype}.txt)
   do
     echo "正在处理${phenotype}_${gene}"
@@ -14,4 +15,4 @@ do
     multpath_2017=/home/riceUsers/lhr/soybean/final_results/haplotype/${phenotype}/2017yzBL/multiple_comparison_result_${gene}_2017yzBL_${phenotype}.csv
     python /home/riceUsers/lhr/soybean/src/my_script/python_script/old/hapdata_for_database.py ${happath_2015} ${happath_2016} ${happath_2017} ${multpath_2015} ${multpath_2016} ${multpath_2017} ${gene} ${phenotype} /home/riceUsers/lhr/soybean/final_results/output/${phenotype}.csv
   done
-done
+#done
